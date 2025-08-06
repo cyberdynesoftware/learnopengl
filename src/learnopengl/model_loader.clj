0e6ac7841b793db9d557d7739295ed08e15b61b7
ingmar <ingmania@protonmail.com>
2025-08-06 15:14:43 -0600

    removed the Java class and some code cleanup

--- src/learnopengl/model_loader.clj
+++ src/learnopengl/model_loader.clj
@@ -17,35 +17,21 @@
    :indices-count indices-count
    :material-index material-index})
 
-(comment copy-to-float-buffer
-  [vertices number dim]
-  (let [buffer (BufferUtils/createFloatBuffer (* number dim))]
-    (doseq [vertex (take number (repeatedly #(.get vertices)))]
-      (.put buffer (.x vertex))
-      (.put buffer (.y vertex))
-      (when (= dim 3)
-        (.put buffer (.z vertex))))
-    (.flip buffer)))
-
-(comment mem-to-float-buffer
-  [buffer size]
-  (.flip (MemoryUtil/memFloatBuffer (.address0 buffer) size)))
-
 (defn create-vertex-buffer
   [^AIMesh mesh]
-  (let [buffer (BufferUtils/createFloatBuffer (* ^int (.mNumVertices mesh) 8))]
-    (dotimes [index ^int (.mNumVertices mesh)]
-      (let [vertex ^AIVector3D (.get ^AIVector3D$Buffer (.mVertices mesh) index)
-            normal ^AIVector3D (.get ^AIVector3D$Buffer (.mNormals mesh) index)
-            tex-coords ^AIVector3D (.get ^AIVector3D$Buffer (.mTextureCoords mesh 0) index)]
-        (.put buffer ^float (.x vertex))
-        (.put buffer ^float (.y vertex))
-        (.put buffer ^float (.z vertex))
-        (.put buffer ^float (.x normal))
-        (.put buffer ^float (.y normal))
-        (.put buffer ^float (.z normal))
-        (.put buffer ^float (.x tex-coords))
-        (.put buffer ^float (.y tex-coords))))
+  (let [buffer (BufferUtils/createFloatBuffer (* (.mNumVertices mesh) 8))]
+    (dotimes [index (.mNumVertices mesh)]
+      (let [vertex ^AIVector3D (.get (.mVertices mesh) index)
+            normal ^AIVector3D (.get (.mNormals mesh) index)
+            tex-coords ^AIVector3D (.get (.mTextureCoords mesh 0) index)]
+        (.put buffer (.x vertex))
+        (.put buffer (.y vertex))
+        (.put buffer (.z vertex))
+        (.put buffer (.x normal))
+        (.put buffer (.y normal))
+        (.put buffer (.z normal))
+        (.put buffer (.x tex-coords))
+        (.put buffer (.y tex-coords))))
     (.flip buffer)))
 
 (defn load-interleaved-vertex-data
@@ -53,7 +39,6 @@
   (GL33/glBindBuffer GL33/GL_ARRAY_BUFFER vbo)
   (let [buffer ^FloatBuffer (create-vertex-buffer aimesh)]
     (GL33/glBufferData GL33/GL_ARRAY_BUFFER buffer GL33/GL_STATIC_DRAW)
-
     (GL33/glVertexAttribPointer 0 3 GL33/GL_FLOAT false 32 0)
     (GL33/glEnableVertexAttribArray 0)
     (GL33/glVertexAttribPointer 1 3 GL33/GL_FLOAT false 32 12)
@@ -61,35 +46,14 @@
     (GL33/glVertexAttribPointer 2 2 GL33/GL_FLOAT false 32 24)
     (GL33/glEnableVertexAttribArray 2)))
 
-(comment load-batched-vertex-data
-  [aimesh vbo]
-  (GL33/glBindBuffer GL33/GL_ARRAY_BUFFER vbo)
-  (let [vertices-size (* (.mNumVertices aimesh) 12)
-        tex-coords-size (* (.mNumVertices aimesh) 8)
-        vertices (mem-to-float-buffer (.mVertices aimesh) vertices-size)
-        normals (mem-to-float-buffer (.mNormals aimesh) vertices-size)
-        tex-coords (mem-to-float-buffer (.mTextureCoords aimesh 0) tex-coords-size)]
-    (GL33/glBufferData GL33/GL_ARRAY_BUFFER (+ (* vertices-size 2) tex-coords-size) GL33/GL_STATIC_DRAW)
-
-    (GL33/glBufferSubData GL33/GL_ARRAY_BUFFER 0 vertices)
-    (GL33/glBufferSubData GL33/GL_ARRAY_BUFFER vertices-size normals)
-    (GL33/glBufferSubData GL33/GL_ARRAY_BUFFER (* vertices-size 2) tex-coords)
-
-    (GL33/glVertexAttribPointer 0 3 GL33/GL_FLOAT false 12 0)
-    (GL33/glEnableVertexAttribArray 0)
-    (GL33/glVertexAttribPointer 1 3 GL33/GL_FLOAT false 12 vertices-size)
-    (GL33/glEnableVertexAttribArray 1)
-    (GL33/glVertexAttribPointer 2 2 GL33/GL_FLOAT false 8 (* vertices-size 2))
-    (GL33/glEnableVertexAttribArray 2)))
-
 (defn create-index-buffer
   [^AIMesh mesh]
-  (let [buffer (BufferUtils/createIntBuffer (* ^int (.mNumFaces mesh) 3))
-        faces ^AIFace$Buffer (.mFaces mesh)]
-    (doseq [^AIFace face (take ^int (.mNumFaces mesh) (repeatedly #(.get faces)))]
-      (assert (= ^int (.mNumIndices face) 3))
+  (let [buffer (BufferUtils/createIntBuffer (* (.mNumFaces mesh) 3))
+        faces (.mFaces mesh)]
+    (doseq [^AIFace face (take (.mNumFaces mesh) (repeatedly #(.get faces)))]
+      (assert (= (.mNumIndices face) 3))
       (let [indices ^IntBuffer (.mIndices face)]
-        (doseq [^int index (take ^int (.mNumIndices face) (repeatedly #(.get indices)))]
+        (doseq [^int index (take (.mNumIndices face) (repeatedly #(.get indices)))]
           (.put buffer index))))
     (.flip buffer)))
 
@@ -108,10 +72,10 @@
     :num-expected 1}])
 
 (defn read-texture-name
-  [material-indices scene]
+  [material-indices ^AIScene scene]
   (assert (= (count material-indices) 1)
           "single material index expected")
-  (let [material-pointer (.get (.mMaterials scene) (first material-indices))
+  (let [material-pointer (.get (.mMaterials scene) ^int (first material-indices))
         material (AIMaterial/create ^long material-pointer)]
     (for [texture-type texture-types]
       (let [texture-count (Assimp/aiGetMaterialTextureCount material (:type texture-type))
@@ -123,7 +87,7 @@
                           (:type texture-type)))
           (Assimp/aiGetMaterialString material
                                       Assimp/_AI_MATKEY_TEXTURE_BASE
-                                      (:type texture-type)
+                                      ^int (:type texture-type)
                                       0
                                       path)
           (.dataString path))))))
@@ -148,8 +112,8 @@
 
 (defn process-scene
   [^AIScene scene]
-  (let [mesh-pointer-buffer ^PointerBuffer (.mMeshes scene)
-        meshes (->> (take ^int (.mNumMeshes scene)
+  (let [mesh-pointer-buffer (.mMeshes scene)
+        meshes (->> (take (.mNumMeshes scene)
                           (repeatedly #(.get mesh-pointer-buffer)))
                     (mapv process-mesh))]
     {:meshes meshes
